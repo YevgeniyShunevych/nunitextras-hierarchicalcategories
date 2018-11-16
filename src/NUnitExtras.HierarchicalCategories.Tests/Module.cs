2c1ddd0ea9613f58bb01609c8965f419426447a6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-11-16 12:04:27 +0200

    Add MultipleAttributesAtFixtureTests

---
+++ src/NUnitExtras.HierarchicalCategories.Tests/Module.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Extras;
+
+namespace NUnitExtras.HierarchicalCategories.Tests
+{
+    public static class Module
+    {
+        public class UI : HierarchicalCategoryAttribute
+        {
+            public class Authentication : HierarchicalCategoryAttribute
+            {
+                public class Admin : HierarchicalCategoryAttribute
+                {
+                }
+
+                public class Guest : HierarchicalCategoryAttribute
+                {
+                }
+            }
+
+            public class Products : HierarchicalCategoryAttribute
+            {
+                public class Purchase : HierarchicalCategoryAttribute
+                {
+                }
+
+                public class Editing : HierarchicalCategoryAttribute
+                {
+                }
+            }
+        }
+
+        [HierarchicalCategoryName("API")]
+        public class Api : HierarchicalCategoryAttribute
+        {
+            public class Authentication : HierarchicalCategoryAttribute
+            {
+                public class Admin : HierarchicalCategoryAttribute
+                {
+                }
+
+                public class Guest : HierarchicalCategoryAttribute
+                {
+                }
+            }
+        }
+    }
+}
---
+++ src/NUnitExtras.HierarchicalCategories.Tests/MultipleAttributesAtFixtureTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NUnitExtras.HierarchicalCategories.Tests
+{
+    [TestFixture]
+    [Module.UI.Authentication.Admin]
+    [Module.UI.Products.Editing]
+    public class MultipleAttributesAtFixtureTests
+    {
+        protected static string[] TestCategories =>
+            TestContext.CurrentContext.Test.Properties[PropertyNames.Category].Cast<string>().ToArray();
+
+        protected static string[] FixtureTestCategories { get; private set; }
+
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            // Currently in NUnit it is possible to get fixture attributes only during [OneTimeSetUp] method.
+            FixtureTestCategories = TestCategories;
+        }
+
+        [Test]
+        public void MultipleAttributesAtFixture_Test()
+        {
+            FixtureTestCategories.Should().BeEquivalentTo(
+                "UI",
+                "UI.Authentication",
+                "UI.Authentication.Admin",
+                "UI.Products",
+                "UI.Products.Editing");
+        }
+
+        [Test]
+        [Module.Api.Authentication.Admin]
+        public void MultipleAttributesAtFixture_WithAttributeAtTest_Test()
+        {
+            FixtureTestCategories.Should().BeEquivalentTo(
+                "UI",
+                "UI.Authentication",
+                "UI.Authentication.Admin",
+                "UI.Products",
+                "UI.Products.Editing");
+
+            TestCategories.Should().BeEquivalentTo(
+                "API",
+                "API.Authentication",
+                "API.Authentication.Admin");
+        }
+    }
+}
